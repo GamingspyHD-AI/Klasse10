1a5bcd421a46fbe7d950301a2ce62b14e2a6081a
GamingspyHD-AI <gamingspyhd@gmail.com>
2026-09-17 08:44:29 +0200

    Add files via upload
    1/3 nochmal und 1/4

--- zufallsliste.py
+++ zufallsliste.py
@@ -7,3 +7,13 @@ for i in range(10):
     zahlen.append(zufallszahl)
 
 print(zahlen)
+
+def durchschnitt(zahlen):
+    if len(zahlen) == 0:
+        return 0
+
+    return sum(zahlen) / len(zahlen)
+
+x = int(input("Möchtest du den Durchschnitt deiner Liste berechnen? (1 für Ja, 0 für Nein): "))
+if x == 1:
+    print("Durchschnitt deiner Liste:", durchschnitt(zahlen))
