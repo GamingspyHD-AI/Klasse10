bcbbe5977b6009a86cae4e8be77047acf03a3326
GamingspyHD-AI <gamingspyhd@gmail.com>
2026-09-17 08:36:38 +0200

    Add files via upload
    1/2 and 1/3

---
+++ schleife.py
@@ -0,0 +1,9 @@
+zahl = 1
+
+while zahl <= 10:
+    if zahl == 5:
+        zahl += 1
+        continue
+
+    print(zahl)
+    zahl += 1
---
+++ zufallsliste.py
@@ -0,0 +1,9 @@
+import random
+
+zahlen = []
+
+for i in range(10):
+    zufallszahl = random.randint(1, 100)
+    zahlen.append(zufallszahl)
+
+print(zahlen)
